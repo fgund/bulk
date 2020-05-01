d5f9ff7cfd37bf3d956187a11e3d0fc733e58d9d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-01 22:27:45 +0000

    Deploy fgund/bulk to github.com/fgund/bulk.git:gh-pages

--- search/all_4.js
+++ search/all_4.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['getbulks',['GetBulks',['../struct_test_logger.html#aeebefd617fe0202445b2a53a6ed964ea',1,'TestLogger']]],
-  ['getresultnotifier',['GetResultNotifier',['../class_bulk.html#af695a982bdbd98ccf966e158bea84044',1,'Bulk']]],
-  ['gettimenotifier',['GetTimeNotifier',['../class_bulk.html#a5569dbf3750d0be5b56b703b6b2b44e4',1,'Bulk']]]
+  ['getresultnotifier',['GetResultNotifier',['../class_bulk.html#a65950e22afd7e41c35d5548349dd523f',1,'Bulk']]],
+  ['gettimenotifier',['GetTimeNotifier',['../class_bulk.html#a82f8bafc7c740c219f36b836134137ca',1,'Bulk']]]
 ];
--- search/all_6.js
+++ search/all_6.js
@@ -1,6 +1,4 @@
 var searchData=
 [
-  ['notify',['Notify',['../struct_i_observable.html#a69b757da498d26bb546af74ed8ba5277',1,'IObservable']]],
-  ['notifyresult',['NotifyResult',['../class_bulk.html#ac1d622fe086e29f1e947c40a0e88eb7c',1,'Bulk']]],
-  ['notifytime',['NotifyTime',['../class_bulk.html#adbd90df7a6ff47f542ff1b7da3deef65',1,'Bulk']]]
+  ['notify',['Notify',['../struct_i_observable.html#a69b757da498d26bb546af74ed8ba5277',1,'IObservable']]]
 ];
