260111440390970bf1c3a3e8c8ad9fb5e2f2a6c3
Obald <f.gundelakh@yandex.ru>
2020-05-02 01:23:05 +0300

    Cleared bulk interface

--- bulk_lib/include/bulk.h
+++ bulk_lib/include/bulk.h
@@ -11,61 +11,15 @@
 class Bulk
 {
 public:
-	Bulk(int size) :bulk_size(size) {
-	}
-	~Bulk() {
-		if (!commands.empty() && !has_nested) {
-			NotifyResult();
-		}
-	}
-	void NotifyResult() {
-		result_notifier.Notify(PrepareOutput());
-	}
-	void NotifyTime() {
-		time_notifier.Notify(std::chrono::system_clock::now());
-	}
-	void Process(std::string&& cmd) {
-		if (cmd == "{") {
-			has_nested = true;
-			++nested_counter;
-		}
-		else if (cmd == "}") {
-			--nested_counter;
-		}
-		else {
-			if (commands.empty()) {
-				NotifyTime();
-			}
-			commands.emplace_back(std::move(cmd));
-		}
-		if ((commands.size() == bulk_size) && (!has_nested)) {
-			NotifyResult();
-			commands.clear();
-		}
-		else if (has_nested && (nested_counter == 0)) {
-			NotifyResult();
-			has_nested = false;
-			commands.clear();
-		}
-	}
-	std::string PrepareOutput() {
-		std::string result{ "bulk: " };
-		auto it = commands.begin();
-		auto end = commands.end();
-		for (; it != end; ++it) {
-			if (it != commands.begin()) {
-				result.append(", ");
-			}
-			result.append(*it);
-		}
-		return std::move(result);
-	}
-	auto& GetResultNotifier() {
-		return result_notifier;
-	}
-	auto& GetTimeNotifier() {
-		return time_notifier;
-	}
+	Bulk(int size);
+	~Bulk();
+	void Process(std::string&& cmd);
+	IObservable<std::string>& GetResultNotifier();
+	IObservable<std::chrono::system_clock::time_point>& GetTimeNotifier();
+private: //methods
+	void NotifyResult();
+	void NotifyTime();
+	std::string PrepareOutput();
 private:
 	bool has_nested{ false };
 	size_t bulk_size;
